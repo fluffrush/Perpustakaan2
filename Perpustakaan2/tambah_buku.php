2d7c757941a9973073cf0eafd08474df578dabd0
dheaalmach <88362752+fluffrush@users.noreply.github.com>
2021-10-05 12:52:50 +0700

    Add files via upload

---
+++ Perpustakaan2/tambah_buku.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+        crossorigin="anonymous">
+        <title></title>
+    </head>
+
+    <body>
+        <h3>Tambah Buku</h3></br>
+        <form action="proses_tambah_buku.php" method="post">
+            Judul buku :
+            <input type="text" name="judul_buku" value="" class="form-control"></br>
+            Penulis :
+            <input type="text" name="penulis" value="" class="form-control"></br>
+            Penerbit :
+            <input type="text" name="penerbit" value="" class="form-control"></br>
+            <input type="submit" name="simpan" value="Tambah Buku" class="btn btn-primary"></br>
+        </form>
+
+        <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+        crossorigin="anonymous"></script>
+    </body>
+    </html>
